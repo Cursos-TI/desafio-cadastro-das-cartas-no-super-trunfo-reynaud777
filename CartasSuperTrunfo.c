1415f2d6bfc355b1bf46168b819ea5c2696dd306
reynaud777 <pauloreynaudneto55@gmail.com>
2025-03-07 19:51:31 +0000

    Corrigi as variáveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,147 +1,26 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
 
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
-    int habitantes1;
-    int habitantes2;
-    int habitantes3;
-    int habitantes4;
-    int habitantes5;
-    int habitantes6;
-    int habitantes7;
-    int habitantes8;
-    int habitantes9;
-    int habitantes10;
-    int habitantes11;
-    int habitantes12;
-    int habitantes13;
-    int habitantes14;
-    int habitantes15;
-    int habitantes16;
-    int habitantes17;
-    int habitantes18;
-    int habitantes19;
-    int habitantes20;
-    int habitantes21;
-    int habitantes22;
-    int habitantes23;
-    int habitantes24;
-    int habitantes25;
-    int habitantes26;
-    int habitantes27;
-    int habitantes28;
-    int habitantes29;
-    int habitantes30;
-    int habitantes31;
-    int habitantes32;
+    #include <stdio.h>
 
-    float area1;
-    float area2;
-    float area3;
-    float area4;
-    float area5;
-    float area6;
-    float area7;
-    float area8;
-    float area9;
-    float area10;
-    float area11;
-    float area12;
-    float area13;
-    float area14;
-    float area15;
-    float area16;
-    float area17;
-    float area18;
-    float area19;
-    float area20;
-    float area21;
-    float area22;
-    float area23;
-    float area24;
-    float area25;
-    float area26;
-    float area27;
-    float area28;
-    float area29;
-    float area30;
-    float area31;
-    float area32;
+int main(){
 
-    int pib1;
-    int pib2;
-    int pib3;
-    int pib4;
-    int pib5;
-    int pib6;
-    int pib7;
-    int pib8;
-    int pib9;
-    int pib10;
-    int pib11;
-    int pib12;
-    int pib13;
-    int pib14;
-    int pib15;
-    int pib16;
-    int pib17;
-    int pib18;
-    int pib19;
-    int pib20;
-    int pib21;
-    int pib22;
-    int pib23;
-    int pib24;
-    int pib25;
-    int pib26;
-    int pib27;
-    int pib28;
-    int pib29;
-    int pib30;
-    int pib31;
-    int pib32;
-
-    int* turismo1;
-    int turismo2;
-    int turismo3;
-    int turismo4;
-    int turismo5;
-    int turismo6;
-    int turismo7;
-    int turismo8;
-    int turismo9;
-    int turismo10;
-    int turismo11;
-    int turismo12;
-    int turismo13;
-    int turismo14;
-    int turismo15;
-    int turismo16;
-    int turismo17;
-    int turismo18;
-    int turismo19;
-    int turismo20;
-    int turismo21;
-    int turismo22;
-    int turismo23;
-    int turismo24;
-    int turismo25;
-    int turismo26;
-    int turismo27;
-    int turismo28;
-    int turismo29;
-    int turismo30;
-    int turismo31;
-    int turismo32;
+    char estado1[20];
+    char estado2[20];
+    char cidade1, cidade2;
+    int codigo1, codigo2;
+    unsigned long int habitantes1, habitantes2;
+    float area1, area2;
+    float pib1, pib2;
+    int turismo1, turismo2;
+    float pibpercapta1;
+    float pibpercapta2;
+    float densidade1;
+    float densidade2;
+    float superpoderA, superpoderB;
+    int resultado1, resultado2, resultado3, resultado4, resultado5, resultado6, resultado7;
 
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
