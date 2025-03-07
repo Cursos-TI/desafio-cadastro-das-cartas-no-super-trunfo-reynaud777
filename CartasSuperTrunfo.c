0e639e72e0dd363f267458015cb0092469aa25e2
reynaud777 <pauloreynaudneto55@gmail.com>
2025-03-07 19:53:09 +0000

    Corrigi a entrada e saída de dados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -26,392 +26,47 @@ int main(){
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
     
-    printf("Desafio Super Trunfo!\n");
-    printf("Cada país será dividido em oito estados, identificados pelas letras de A a H.\nCada estado terá quatro cidades, numeradas de 1 a 4.\n");
+    printf("Qual será o nome do primeiro estado?\n");
+    scanf("%s", &estado1);
     
-    printf("Insira quantos habitantes haverão na carta A1:\n");
+    printf("Digite qual letra deve representar a cidade 1 do estado: %s\n", estado1);
+    scanf(" %c", &cidade1);
+
+    printf("Digite qual número irá compor o código da cidade 1:\n");
+    scanf(" %d", &codigo1);
+
+    printf("Digite o número de habitantes:\n");
     scanf("%d", &habitantes1);
-
-    printf("Insira quantos habitantes haverão na carta A2:\n");
-    scanf("%d", &habitantes2);
-
-    printf("Insira quantos habitantes haverão na carta A3:\n");
-    scanf(" %d", &habitantes3);
-
-    printf("Insira quantos habitantes haverão na carta A4:\n");
-    scanf(" %d", &habitantes4);
-
-    printf("Qual será a área da carta A1?\n");
+    
+    printf("Qual será a área da carta %c%d?\n", cidade1, codigo1);
     scanf("%f", &area1);
 
-    printf("Qual será a área da carta A2?\n");
+    printf("Digite o PIB:\n");
+    scanf(" %f", &pib1);
+
+    printf("Quantos pontos turísticos terá na cidade?\n");
+    scanf("%d", &turismo1);
+
+    printf("Qual será o nome do segundo estado?\n");
+    scanf(" %s", &estado2);
+
+    printf("Digite qual letra deve representar a cidade 1 do estado: %s\n", estado2);
+    scanf(" %c", &cidade2);
+    
+    printf("Digite qual número irá compor o código da cidade 1:\n");
+    scanf(" %d", &codigo2);
+
+    printf("Digite o número de habitantes:\n");
+    scanf("%d", &habitantes2);
+    
+    printf("Qual será a área da carta %c%d?\n", cidade2, codigo2);
     scanf("%f", &area2);
 
-    printf("Qual será a área da carta A3?\n");
-    scanf("%f", &area3);
+    printf("Digite o PIB:\n");
+    scanf("%f", &pib2);
 
-    printf("Qual será a área da carta A4?\n");
-    scanf("%f", &area4);
-
-    printf("Digite qual será o PIB da carta A1:\n");
-    scanf("%d", &pib1);
-
-    printf("Digite qual será o PIB da carta A2:\n");
-    scanf("%d", &pib2);
-
-    printf("Digite qual será o PIB da carta A3:\n)");
-    scanf("%d", &pib3);
-
-    printf("Digite qual será o PIB da carta A4:\n");
-    scanf("%d", &pib4);
-
-    printf("Quantos pontos turísticos terá em A1?\n");
-    scanf("%d", turismo1);
-
-    printf("Quantos pontos turísticos terá em A2?\n");
+    printf("Quantos pontos turísticos terá na cidade?\n");
     scanf("%d", &turismo2);
-
-    printf("Quantos pontos turísticos terá em A3?\n");
-    scanf("%d", &turismo3);
-
-    printf("Quantos pontos turísticos terá em A4?\n");
-    scanf("%d", &turismo4);
-
-    printf("Insira quantos habitantes haverão na carta B1:\n");
-    scanf(" %d", &habitantes5);
-
-    printf("Insira quantos habitantes haverão na carta B2:\n");
-    scanf("%d", &habitantes6);
-
-    printf("Insira quantos habitantes haverão na carta B3:\n");
-    scanf("%d", &habitantes7);
-
-    printf("Insira quantos habitantes haverão na carta B4:\n");
-    scanf("%d", &habitantes8);
-
-    printf("Qual será a área da carta B1?\n");
-    scanf("%f", &area5);
-
-    printf("Qual será a área da carta B2?\n");
-    scanf("%f", &area6);
-
-    printf("Qual será a área da carta B3?\n");
-    scanf("%f", &area7);
-
-    printf("Qual será a área da carta B4?\n");
-    scanf("%f", &area8);
-
-    printf("Digite qual será o PIB da carta B1:\n");
-    scanf("%d", &pib5);
-
-    printf("Digite qual será o PIB da carta B2:\n");;
-    scanf("%d", &pib6);
-
-    printf("Digite qual será o PIB da carta B3:\n");
-    scanf("%d", &pib7);
-
-    printf("Digite qual será o PIB da carta B4:\n");
-    scanf("%d", &pib8);
-
-    printf("Quantos pontos turísticos terá em B1?\n");
-    scanf("%d", &turismo5);
-
-    printf("Quantos pontos turísticos terá em B2?\n");
-    scanf("%d", &turismo6);
-
-    printf("Quantos pontos turísticos terá em B3?\n");
-    scanf("%d", &turismo7);
-
-    printf("Quantos pontos turísticos terá em B4?\n");
-    scanf("%d", &turismo8);
-
-    printf("Insira quantos habitantes haverão na carta C1:\n");
-    scanf("%d", &habitantes9);
-
-    printf("Insira quantos habitantes haverão na carta C2:\n");
-    scanf("%d", &habitantes10);
-
-    printf("Insira quantos habitantes haverão na carta C3:\n");
-    scanf("%d", &habitantes11);
-
-    printf("Insira quantos habitantes haverão na carta C4:\n");
-    scanf("%d", &habitantes12);
-
-    printf("Qual será a área da carta C1?\n");
-    scanf(" %f", &area9);
-
-    printf("Qual será a área da carta C2?\n");
-    scanf("%f", &area10);
-
-    printf("Qual será a área da carta C3?\n");
-    scanf("%f", &area11);
-
-    printf("Qual será a área da carta C4?\n");
-    scanf("%f", &area12);
-
-    printf("Digite qual será o PIB da carta C1:\n");
-    scanf("%d", &pib9);
-
-    printf("Digite qual será o PIB da carta C2:\n");
-    scanf("%d", &pib10);
-
-    printf("Digite qual será o PIB da carta C3:\n");
-    scanf("%d", &pib11);
-
-    printf("Digite qual será o PIB da carta C4:\n");
-    scanf("%d", &pib12);
-    
-    printf("Quantos pontos turísticos terá em C1?\n");
-    scanf("%d", &turismo9);
-
-    printf("Quantos pontos turísticos terá em C2?\n");
-    scanf("%d", &turismo10);
-
-    printf("Quantos pontos turísticos terá em C3?\n");
-    scanf("%d", &turismo11);
-
-    printf("Quantos pontos turísticos terá em C4?\n");
-    scanf("%d", &turismo12);
-
-    printf("Insira quantos habitantes haverão na carta D1:\n");
-    scanf("%d", &habitantes13);
-
-    printf("Insira quantos habitantes haverão na carta D2:\n");
-    scanf("%d", &habitantes14);
-
-    printf("Insira quantos habitantes haverão na carta D3:\n");
-    scanf("%d", &habitantes15);
-
-    printf("Insira quantos habitantes haverão na carta D4:\n");
-    scanf("%d", &habitantes16);
-
-    printf("Qual será a área da carta D1?\n");
-    scanf("%f", &area13);
-
-    printf("Qual será a área da carta D2?\n");
-    scanf("%f", &area14);
-
-    printf("Qual será a área da carta D3?\n");
-    scanf("%f", &area15);
-
-    printf("Qual será a área da carta D4?\n");
-    scanf("%f", &area16);
-
-    printf("Digite qual será o PIB da carta D1:\n");
-    scanf("%d", &pib13);
-
-    printf("Digite qual será o PIB da carta D2:\n");
-    scanf("%d", &pib14);
-
-    printf("Digite qual será o PIB da carta D3:\n");
-    scanf("%d", &pib15);
-
-    printf("Digite qual será o PIB da carta D4:\n");
-    scanf("%d", &pib16);
-    
-    printf("Quantos pontos turísticos terá em D1?\n");
-    scanf("%d", &turismo13);
-
-    printf("Quantos pontos turísticos terá em D2?\n");
-    scanf("%d", &turismo14);
-
-    printf("Quantos pontos turísticos terá em D3?\n");
-    scanf("%d", &turismo15);
-
-    printf("Quantos pontos turísticos terá em D4?\n");
-    scanf("%d", &turismo16);
-
-    printf("Insira quantos habitantes haverão na carta E1:\n");
-    scanf("%d", &habitantes17);
-
-    printf("Insira quantos habitantes haverão na carta E2:\n");
-    scanf("%d", &habitantes18);
-
-    printf("Insira quantos habitantes haverão na carta E3:\n");
-    scanf("%d", &habitantes19);
-
-    printf("Insira quantos habitantes haverão na carta E4:\n");
-    scanf("%d", &habitantes20);
-
-    printf("Qual será a área da carta E1?\n");
-    scanf("%f", &area17);
-
-    printf("Qual será a área da carta E2?\n");
-    scanf("%f", &area18);
-
-    printf("Qual será a área da carta E3?\n");
-    scanf("%f", &area19);
-
-    printf("Qual será a área da carta E4?\n");
-    scanf("%f", &area20);
-
-    printf("Digite qual será o PIB da carta E1:\n");
-    scanf("%d", &pib17);
-
-    printf("Digite qual será o PIB da carta E2:\n");
-    scanf("%d", &pib18);
-
-    printf("Digite qual será o PIB da carta E3:\n");
-    scanf("%d", &pib19);
-
-    printf("Digite qual será o PIB da carta E4:\n");
-    scanf("%d", &pib20);
-    
-    printf("Quantos pontos turísticos terá em E1?\n");
-    scanf("%d", &turismo17);
-
-    printf("Quantos pontos turísticos terá em E2?\n");
-    scanf("%d", &turismo18);
-
-    printf("Quantos pontos turísticos terá em E3?\n");
-    scanf("%d", &turismo19);
-
-    printf("Quantos pontos turísticos terá em E4?\n");
-    scanf("%d", &turismo20);
-
-    printf("Insira quantos habitantes haverão na carta F1:\n");
-    scanf("%d", &habitantes21);
-
-    printf("Insira quantos habitantes haverão na carta F2:\n");
-    scanf("%d", &habitantes22);
-
-    printf("Insira quantos habitantes haverão na carta F3:\n");
-    scanf("%d", &habitantes23);
-
-    printf("Insira quantos habitantes haverão na carta F4:\n");
-    scanf("%d", &habitantes24);
-
-    printf("Qual será a área da carta F1?\n");
-    scanf("%f", &area21);
-
-    printf("Qual será a área da carta F2?\n");
-    scanf("%f", &area22);
-
-    printf("Qual será a área da carta F3?\n");
-    scanf("%f", &area23);
-
-    printf("Qual será a área da carta F4?\n");
-    scanf("%f", &area24);
-
-    printf("Digite qual será o PIB da carta F1:\n");
-    scanf("%d", &pib21);
-
-    printf("Digite qual será o PIB da carta F2:\n");
-    scanf("%d", &pib22);
-
-    printf("Digite qual será o PIB da carta F3:\n");
-    scanf("%d", &pib23);
-
-    printf("Digite qual será o PIB da carta F4:\n");
-    scanf("%d", &pib24);
-    
-    printf("Quantos pontos turísticos terá em F1?\n");
-    scanf("%d", &turismo21);
-
-    printf("Quantos pontos turísticos terá em F2?\n");
-    scanf("%d", &turismo22);
-
-    printf("Quantos pontos turísticos terá em F3?\n");
-    scanf("%d", &turismo23);
-
-    printf("Quantos pontos turísticos terá em F4?\n");
-    scanf("%d", &turismo24);
-
-    printf("Insira quantos habitantes haverão na carta G1:\n");
-    scanf("%d", &habitantes25);
-
-    printf("Insira quantos habitantes haverão na carta G2:\n");
-    scanf("%d", &habitantes26);
-
-    printf("Insira quantos habitantes haverão na carta G3:\n");
-    scanf("%d", &habitantes27);
-
-    printf("Insira quantos habitantes haverão na carta G4:\n");
-    scanf("%d", &habitantes28);
-
-    printf("Qual será a área da carta G1?\n");
-    scanf("%f", &area25);
-
-    printf("Qual será a área da carta G2?\n");
-    scanf("%f", &area26);
-
-    printf("Qual será a área da carta G3?\n");
-    scanf("%f", &area27);
-
-    printf("Qual será a área da carta G4?\n");
-    scanf("%f", &area28);
-
-    printf("Digite qual será o PIB da carta G1:\n");
-    scanf("%d", &pib25);
-
-    printf("Digite qual será o PIB da carta G2:\n");
-    scanf("%d", &pib26);
-
-    printf("Digite qual será o PIB da carta G3:\n");
-    scanf("%d", &pib27);
-
-    printf("Digite qual será o PIB da carta G4:\n");
-    scanf("%d", &pib28);
-    
-    printf("Quantos pontos turísticos terá em G1?\n");
-    scanf("%d", &turismo25);
-
-    printf("Quantos pontos turísticos terá em G2?\n");
-    scanf("%d", &turismo26);
-
-    printf("Quantos pontos turísticos terá em G3?\n");
-    scanf("%d", &turismo27);
-
-    printf("Quantos pontos turísticos terá em G4?\n");
-    scanf("%d", &turismo28);
-
-    printf("Insira quantos habitantes haverão na carta H1:\n");
-    scanf("%d", &habitantes29);
-
-    printf("Insira quantos habitantes haverão na carta H2:\n");
-    scanf("%d", &habitantes30);
-
-    printf("Insira quantos habitantes haverão na carta H3:\n");
-    scanf("%d", &habitantes31);
-
-    printf("Insira quantos habitantes haverão na carta H4:\n");
-    scanf("%d", &habitantes32);
-
-    printf("Qual será a área da carta H1?\n");
-    scanf("%f", &area29);
-
-    printf("Qual será a área da carta H2?\n");
-    scanf("%f", &area30);
-
-    printf("Qual será a área da carta H3?\n");
-    scanf("%f", &area31);
-
-    printf("Qual será a área da carta H4?\n");
-    scanf("%f", &area32);
-
-    printf("Digite qual será o PIB da carta H1:\n");
-    scanf("%d", &pib29);
-
-    printf("Digite qual será o PIB da carta H2:\n");
-    scanf("%d", &pib30);
-
-    printf("Digite qual será o PIB da carta H3:\n");
-    scanf("%d", &pib31);
-
-    printf("Digite qual será o PIB da carta H4:\n");
-    scanf("%d", &pib32);
-    
-    printf("Quantos pontos turísticos terá em H1?\n");
-    scanf("%d", &turismo29);
-
-    printf("Quantos pontos turísticos terá em H2?\n");
-    scanf("%d", &turismo30);
-
-    printf("Quantos pontos turísticos terá em H3?\n");
-    scanf("%d", &turismo31);
-
-    printf("Quantos pontos turísticos terá em H4?\n");
-    scanf("%d", &turismo32);
 
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
